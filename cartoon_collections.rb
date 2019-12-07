7dc1a9c31f55fb7f05e2eee0d736610d5ee5623c
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-07 21:59:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves
+ dwarves.collect { |dwarve| print darf + "\n"} 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  array.map {|name| name.capitalize }
+ array.map {|name| print #{name.capitalize}!} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  if calls_long.include? {|name| > 4.characters} 
+    puts true 
+  else 
+    puts false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include? {|name| cheese_types}
+    puts array.cheese_types 
+  else 
+    puts nil 
 end
