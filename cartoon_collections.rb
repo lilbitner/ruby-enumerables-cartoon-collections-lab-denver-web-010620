174c3fde9ac9d6009ab0f4e46e0d6a69044f4e6a
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-07 21:43:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,4 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves
  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
  dwarves.each { |dwarve| print darf + "\n"} 
 end
