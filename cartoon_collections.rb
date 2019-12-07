deaf8e742ca1c9316b60556dcff6efd178626123
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-07 21:47:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"] do
-  planeteer_calls.capitlize { |planeteer| "#Planeteer!"}
+  planeteer_calls.capitlize { |planeteer| print "#Planeteer!"}
 end
 
 def long_planeteer_calls# code an argument here
