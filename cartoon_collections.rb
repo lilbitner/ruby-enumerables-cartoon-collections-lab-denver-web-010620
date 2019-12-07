8ae7f6e994ef99e347190da0095e189711b01749
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-07 21:42:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves# code an argument here
   # Your code here
-  
+ dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+ dwarves.each []
 end
 
 def summon_captain_planet# code an argument here
